585da2ba85a1738bb24eab4277ea4ce2db9e1216
Bhagya Mudgal <bhagyamudgal@gmail.com>
2023-12-30 23:25:32 +0530

    fix: sol wallet address validation

--- src/utils/solana.ts
+++ src/utils/solana.ts
@@ -4,10 +4,6 @@ import { logError } from "./general";
 
 export const isValidSolanaAddress = (address: string) => {
     try {
-        if (address?.length !== 44) {
-            return false;
-        }
-
         const pubKey = new PublicKey(address);
 
         return !!pubKey;
